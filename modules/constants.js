54919df0390b4cfc0dab69162ac3454e5b713940
Blake Mealey <blakemealey@gmail.com>
2019-05-04 12:08:20 -0600

    Improved URL matching

--- modules/constants.js
+++ modules/constants.js
@@ -5,5 +5,5 @@ export const DEFAULT_FORMAT_STRING = `Committed to [\${branch.name}|\${branch.ur
 
 export const DEFAULT_BUILD_FORMAT_STRING = `* \${build.name} Build: [\${build.id}|\${build.url}]\n`;
 
-export const COMMIT_URL_REGEX = /^https:\/\/(dev\.azure\.com|.+\.visualstudio\.com)(\/.+\/_git\/.+\/commit\/\b[0-9a-f]{5,40}\b)\/?$/;
+export const COMMIT_URL_REGEX = /^https:\/\/(dev\.azure\.com|.+\.visualstudio\.com)(\/.+\/_git\/.+\/commit\/\b[0-9a-f]{5,40}\b).*$/;
 export const BUILD_URL_REGEX = /^https:\/\/(dev\.azure\.com|.+\.visualstudio\.com)(.+\/_build(\/|%2F)results\/?\?buildId=.+)$/;
