0b99cdad1b7ee17786af7b415854c7f1d82b03e6
Blake Mealey <blakemealey@gmail.com>
2019-05-04 12:50:50 -0600

    Moved copy logic to popup, added popup status

--- background.js
+++ background.js
@@ -1,45 +1,4 @@
-import { DEFAULT_BUILD_FORMAT_STRING, DEFAULT_FORMAT_STRING, BUILD_URL_REGEX, COMMIT_URL_REGEX } from './modules/constants.js';
-import { Formatter } from './modules/formatter.js';
-
-function copyToClipboard(text) {
-    const input = document.createElement('textarea');
-    input.style.position = 'fixed';
-    input.style.opacity = 0;
-    input.value = text;
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand('Copy');
-    document.body.removeChild(input);
-};
-
-const buildsStringFormatter = new Formatter('builds');
-const commitStringFormatter = new Formatter('commit');
-
-async function formatStrings(context) {
-    return new Promise((resolve) => {
-        chrome.storage.sync.get({
-            buildFormatString: DEFAULT_BUILD_FORMAT_STRING,
-            formatString: DEFAULT_FORMAT_STRING
-        }, async (items) => {
-            const buildsString = await buildsStringFormatter.formatString(items.buildFormatString, context.builds);
-            const result = await commitStringFormatter.formatString(items.formatString, {
-                branch: context.branch,
-                pullRequest: context.pullRequest,
-                commit: context.commit,
-                buildsString
-            });
-
-            resolve(result);
-        });
-    });
-}
-
-async function processData(context) {
-    const result = await formatStrings(context);
-
-    console.log(result);
-    copyToClipboard(result);
-}
+import { BUILD_URL_REGEX, COMMIT_URL_REGEX } from './modules/constants.js';
 
 function isBuildResultsPage(url) {
     return BUILD_URL_REGEX.test(url);
@@ -48,20 +7,6 @@ function isBuildResultsPage(url) {
 function isCommitPage(url) {
     return COMMIT_URL_REGEX.test(url);
 }
-
-chrome.pageAction.onClicked.addListener((tab) => {
-    if (isCommitPage(tab.url)) {
-        chrome.tabs.executeScript(null, {
-            file: 'getData.js'
-        });
-    }
-});
-
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.request === 'returnData') {
-        processData(message.result);
-    }
-});
 
 function updateBrowserActionVisibility(tab) {
     if (isBuildResultsPage(tab.url) || isCommitPage(tab.url)) {
---
+++ popup/popup.js
@@ -0,0 +1,68 @@
+import { DEFAULT_BUILD_FORMAT_STRING, DEFAULT_FORMAT_STRING } from '../modules/constants.js';
+import { Formatter } from '../modules/formatter.js';
+
+function updateCopyStatus(text, state) {
+    const copyStatusElement = document.getElementById('copy-status');
+    copyStatusElement.textContent = text;
+    copyStatusElement.className = '';
+    copyStatusElement.classList.add('tag', `bd-${state}`, `text-${state}`);
+}
+
+function copyToClipboard(text) {
+    const input = document.createElement('textarea');
+    input.style.position = 'fixed';
+    input.style.opacity = 0;
+    input.style.pointerEvents = 'none';
+    input.value = text;
+
+    document.body.appendChild(input);
+    input.select();
+
+    document.execCommand('Copy');
+    document.body.removeChild(input);
+};
+
+const sandboxPath = '../sandbox.html';
+const buildsStringFormatter = new Formatter('builds', sandboxPath);
+const commitStringFormatter = new Formatter('commit', sandboxPath);
+
+async function formatStrings(context) {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get({
+            buildFormatString: DEFAULT_BUILD_FORMAT_STRING,
+            formatString: DEFAULT_FORMAT_STRING
+        }, async (items) => {
+            const buildsString = await buildsStringFormatter.formatString(items.buildFormatString,
+                context.builds);
+
+            const result = await commitStringFormatter.formatString(items.formatString, {
+                branch: context.branch,
+                pullRequest: context.pullRequest,
+                commit: context.commit,
+                buildsString
+            });
+
+            resolve(result);
+        });
+    });
+}
+
+async function processData(context) {
+    const result = await formatStrings(context);
+
+    updateCopyStatus('Copied', 'success');
+
+    console.log(result);
+    copyToClipboard(result);
+}
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.request === 'returnData') {
+        processData(message.result);
+    }
+});
+
+updateCopyStatus('Copying...', 'grey');
+chrome.tabs.executeScript(null, {
+    file: 'getData.js'
+});
