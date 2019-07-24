069c0d2051f687f1ca8ca6f3a8567180c51b3e41
Blake Mealey <blakemealey@gmail.com>
2019-07-24 10:54:44 -0600

    Use markdown syntax in default format strings

--- modules/constants.js
+++ modules/constants.js
@@ -1,9 +1,9 @@
-export const DEFAULT_FORMAT_STRING = `Committed to [\${branch.name}|\${branch.url}]:
-* Pull Request: [\${pullRequest.id}|\${pullRequest.url}]
-* Commit: [\${commit.shortHash}|\${commit.url}]
+export const DEFAULT_FORMAT_STRING = `Committed to [\${branch.name}](\${branch.url}):
+* Pull Request: [\${pullRequest.id}](\${pullRequest.url})
+* Commit: [\${commit.shortHash}](\${commit.url})
 \${buildsString}`;
 
-export const DEFAULT_BUILD_FORMAT_STRING = `* \${build.name} Build: [\${build.id}|\${build.url}]\n`;
+export const DEFAULT_BUILD_FORMAT_STRING = `* \${build.name} Build: [\${build.id}](\${build.url})\n`;
 
 export const COMMIT_URL_REGEX = /^https:\/\/(dev\.azure\.com|.+\.visualstudio\.com)(\/.+\/_git\/.+\/commit\/\b[0-9a-f]{5,40}\b).*$/;
 export const BUILD_URL_REGEX = /^https:\/\/(dev\.azure\.com|.+\.visualstudio\.com)(.+\/_build(\/|%2F)results\/?\?buildId=.+)$/;
